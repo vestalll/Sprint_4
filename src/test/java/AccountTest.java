c870828064db5c32b346fea5a5067e321dfb5777
vestalll <vesta_liplianina@yandex.ru>
2022-05-03 22:05:22 +0300

    add parametrization to tests

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,86 +1,42 @@
 import io.qameta.allure.junit4.DisplayName;
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class AccountTest {
 
-    @Test
-    @DisplayName("Можно напечатать строку с фамилией и именем с допустимым значением")
-    public void checkNameToEmbossWithValidData() {
-        String name = "Vasya Ivanov";
-        Account account = new Account(name);
-        Assert.assertTrue(account.checkNameToEmboss());
+    private final String name;
+    private final boolean expected;
+
+    public AccountTest(String name, boolean expected) {
+        this.name = name;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getName() {
+        return new Object[][]{
+                {"Vasya Ivanov", true},
+                {"A C", true},
+                {"Qwertyui Asdfghjkl", true},
+                {"Ab", false},
+                {"Qwertyuio Asdfghjklz", false},
+                {"Vasya  Ivanov", false},
+                {" Vasya Ivanov", false},
+                {"Vasya Ivanov ", false},
+                {"", false},
+                {null, false},
+
+        };
     }
 
     @Test
-    @DisplayName("Можно напечатать строку с фамилией и именем с минимальным кол-вом символов")
-    public void checkNameToEmbossWithMinLength() {
-        String name = "A C";
+    @DisplayName("Проверка возможности печати строки с именем и фамилией")
+    public void checkNameToEmbossTest() {
         Account account = new Account(name);
-        Assert.assertTrue(account.checkNameToEmboss());
-    }
-
-    @Test
-    @DisplayName("Нельзя напечатать строку с фамилией и именем с кол-вом символов меньше минимального")
-    public void checkNameToEmbossWithLessThanMinLength() {
-        String name = "Ab";
-        Account account = new Account(name);
-        Assert.assertFalse(account.checkNameToEmboss());
-    }
-
-    @Test
-    @DisplayName("Можно напечатать строку с фамилией и именем с максимальным кол-вом символов")
-    public void checkNameToEmbossWithMaxLength() {
-        String name = "Qwertyui Asdfghjkl";
-        Account account = new Account(name);
-        Assert.assertTrue(account.checkNameToEmboss());
-    }
-
-    @Test
-    @DisplayName("Нельзя напечатать строку с фамилией и именем с кол-вом символов больше максимального")
-    public void checkNameToEmbossWithMoreThanMaxLength() {
-        String name = "Qwertyuio Asdfghjklz";
-        Account account = new Account(name);
-        Assert.assertFalse(account.checkNameToEmboss());
-    }
-
-    @Test
-    @DisplayName("Нельзя напечатать строку с фамилией и именем с более чем одним пробелом внутри")
-    public void checkNameToEmbossWithMoreThanOneSpace() {
-        String name = "Vasya  Ivanov";
-        Account account = new Account(name);
-        Assert.assertFalse(account.checkNameToEmboss());
-    }
-
-    @Test
-    @DisplayName("Нельзя напечатать строку с фамилией и именем с пробелом в начале строки")
-    public void checkNameToEmbossStartsFromSpace() {
-        String name = " VasyaIvanov";
-        Account account = new Account(name);
-        Assert.assertFalse(account.checkNameToEmboss());
-    }
-
-    @Test
-    @DisplayName("Нельзя напечатать строку с фамилией и именем с пробелом в конце строки")
-    public void checkNameToEmbossEndsWithSpace() {
-        String name = "VasyaIvanov ";
-        Account account = new Account(name);
-        Assert.assertFalse(account.checkNameToEmboss());
-    }
-
-    @Test
-    @DisplayName("Нельзя напечатать строку с пустым значением")
-    public void checkNameToEmbossWithEmptyString() {
-        String name = "";
-        Account account = new Account(name);
-        Assert.assertFalse(account.checkNameToEmboss());
-    }
-
-    @Test
-    @DisplayName("Нельзя напечатать строку в случае если задано null вместо значения")
-    public void checkNameToEmbossWithNull() {
-        String name = null;
-        Account account = new Account(name);
-        Assert.assertFalse(account.checkNameToEmboss());
+        assertEquals(expected, account.checkNameToEmboss());
     }
 }
